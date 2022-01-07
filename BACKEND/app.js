b2fde427116a82452e6e6467e1768ffba5769a80
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-07 19:48:28 +0100

    Starting With The Backend: - Create First API Call & Parsing Json Data & tested by postman

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const app = express();
 
@@ -5,13 +6,27 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get(api+'/products', (req, res) =>{
-    res.send('Hello API!');
+//Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) =>{
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image:'some_url'
+    }
+    res.send(product);
 })
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+
+    res.send(newProduct);
+});
 
 
 app.listen(3000, ()=>{
-    console.log(api);
     console.log("The server is running now on http://localhost:3000");
 });
 
