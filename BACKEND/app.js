4400198fbb2381e94826746ee64f9c024e16c494
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-07 20:03:27 +0100

    Starting With The Backend: -m Logging API Requests with morgan library

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,6 +1,7 @@
 const { request } = require('express');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 require('dotenv/config');
 
@@ -8,6 +9,7 @@ const api = process.env.API_URL;
 
 //Middleware
 app.use(express.json());
+app.use(morgan('tiny'));
 
 app.get(`${api}/products`, (req, res) =>{
     const product = {
