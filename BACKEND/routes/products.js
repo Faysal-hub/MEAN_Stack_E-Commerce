c96047ef0ab18c0ceb6fff8147e05412c74ba4d3
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-10 04:37:05 +0100

    Backend: Products & Categories: - Post a New Product REST API

--- BACKEND/routes/products.js
+++ BACKEND/routes/products.js
@@ -1,5 +1,6 @@
 const { Product } = require('../models/product');
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 
 
@@ -12,27 +13,33 @@ router.get(`/`, async (req, res) => {
   res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
+router.post(`/`, async (req, res) => {
+  const catagory = await Category.findById(req.body.catagory);
+  if(!catagory) return res.status(400).send('Invalid Category')
+
   const product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    catagory: req.body.catagory,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeature: req.body.isFeature,
   });
 
-  
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
-});
+  product = await product.save();
 
+  if (!product) {
+    return res.status(500).send('The product can not be created!')
+  }
+
+  res.send(product);
+
+});
 
 module.exports = router;
 
