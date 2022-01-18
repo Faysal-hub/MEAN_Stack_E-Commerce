32e4101b8bfcef793baafe9242daaefad54b2de8
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-18 04:46:37 +0100

    Backend: Users & Authentication: - set the secret in .env file & token validation

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -78,13 +78,14 @@ router.put('/:id', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   const user = await User.findOne({email: req.body.email})
+  const secret = process.env.secret;
 
   if (!user) {
     return res.status(400).send('The user not found!');
   }
 
   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-     const token = jwt.sign({ userId: user.id }, 'secret' )
+     const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1d' });
         res.status(200).send({user: user.email, token: token}) 
     } else {
         res.status(400).send('password is wrong!');
