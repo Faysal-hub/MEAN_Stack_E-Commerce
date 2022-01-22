38dd7fdb06dacbb1b12408db06b7dbb501dcdf1f
Faysal Ibn Mizan <ovitahsan@gmail.com>
2022-01-22 05:24:54 +0100

    Backend: Users & Authentication: - Add More Secret User Information to Token.

--- BACKEND/routes/users.js
+++ BACKEND/routes/users.js
@@ -85,8 +85,15 @@ router.post('/login', async (req, res) => {
   }
 
   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-     const token = jwt.sign({ userId: user.id }, secret, { expiresIn: '1d' });
-        res.status(200).send({user: user.email, token: token}) 
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        isAdmin: user.isAdmin,
+      },
+      secret,
+      { expiresIn: '1d' }
+    );
+      res.status(200).send({user: user.email, token: token}) 
     } else {
         res.status(400).send('password is wrong!');
   }
